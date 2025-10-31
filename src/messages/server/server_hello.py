1adb194b8bb0ce0ccf5339265768d42b65adc90f
L a n t e r n : ) <yuju32577@gmail.com>
2025-10-31 12:44:09 +0900

    Update ServerHelloMessage to read 32-byte pass token

--- src/messages/server/server_hello.py
+++ src/messages/server/server_hello.py
@@ -1,10 +1,11 @@
 from ...bytestream import ByteStream
 
 class ServerHelloMessage:
-    packet_type = 20100 # TODO: same as login ok??
+    packet_type = 20100
 
     def __init__(self):
         self.bytestream = ByteStream()
+        self.pass_token: bytes = b''  # 패스 토큰을 저장할 변수 추가
     
     def decode(self):
-        pass
+        self.pass_token = self.bytestream.readBytes(32)  # 32바이트 패스 토큰 읽기
