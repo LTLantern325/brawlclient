3653a11c4df13ba0a012a608a89b22d6d4296361
L a n t e r n : ) <yuju32577@gmail.com>
2025-10-31 22:21:10 +0900

    Update LoginMessage to use attested_token with writeString

--- src/messages/client/login_m.py
+++ src/messages/client/login_m.py
@@ -3,11 +3,13 @@ from ...bytestream import ByteStream
 class LoginMessage:
     def __init__(self):
         self.bytestream = ByteStream()
-    
+
     def encode(self, client):
         self.bytestream.writeInt(client.hi or 0) # high
         self.bytestream.writeInt(client.lo or 0) # low
-        self.bytestream.writeString(client.token) # token
+
+        # attest 함수로 생성된 80바이트 토큰을 writeString으로 씁니다.
+        self.bytestream.writeString(client.attested_token.decode('latin-1'))
 
         self.bytestream.writeInt(client.settings["major"])
         self.bytestream.writeInt(client.settings["build"])
